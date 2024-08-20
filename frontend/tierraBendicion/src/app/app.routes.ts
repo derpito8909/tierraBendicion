e8bd49453789cbfba00453c290cccc2c78e026b5
DavidRodriguez <davisito544@gmail.com>
2024-08-19 19:35:14 -0500

    [Actualizacion] modificacion de rutas y creacion de estilos para el inicio de sesion

--- frontend/tierraBendicion/src/app/app.routes.ts
+++ frontend/tierraBendicion/src/app/app.routes.ts
@@ -1,9 +1,7 @@
 import { Routes } from '@angular/router';
 import { InicioComponent } from './pages/inicio/inicio.component';
-import { IngresoComponent } from './pages/ingreso/ingreso.component';
-import { RegistroComponent } from './pages/registro/registro.component';
 import { NoEncontradoComponent } from './pages/no-encontrado/no-encontrado.component';
-import { PrincipalComponent } from './pages/principal/principal.component';
+
 import { PersonasNuevasComponent } from './pages/personas-nuevas/personas-nuevas.component';
 import { ConocenosComponent } from './pages/conocenos/conocenos.component';
 import { ActividadesEspecialesComponent } from './pages/actividades-especiales/actividades-especiales.component';
@@ -13,18 +11,6 @@ export const routes: Routes = [
   {
     path: 'inicio',
     component: InicioComponent,
-  },
-  {
-    path: 'principal',
-    component: PrincipalComponent,
-  },
-  {
-    path: 'ingreso',
-    component: IngresoComponent,
-  },
-  {
-    path: 'registro',
-    component: RegistroComponent,
   },
   {
     path: 'personasNuevas',
--- frontend/tierraBendicion/src/app/pages/inicio/inicio.component.ts
+++ frontend/tierraBendicion/src/app/pages/inicio/inicio.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { PiePaginaComponent } from '../../components/pie-pagina/pie-pagina.component';
 
 @Component({
   selector: 'app-inicio',
   standalone: true,
-  imports: [RouterLink, PiePaginaComponent],
+  imports: [RouterLink],
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.css',
 })
