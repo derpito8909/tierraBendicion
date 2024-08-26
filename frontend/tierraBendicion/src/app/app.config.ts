8ffe560872b36fbc40e0c12a911175400fc1060b
DavidRodriguez <davisito544@gmail.com>
2024-08-26 02:36:22 -0500

    [Actualizacion] configuracion de los interceptadores para validar y autenticar

--- frontend/tierraBendicion/src/app/app.config.ts
+++ frontend/tierraBendicion/src/app/app.config.ts
@@ -1,8 +1,29 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import {
+  provideHttpClient,
+  withInterceptorsFromDi,
+  HTTP_INTERCEPTORS,
+} from '@angular/common/http';
+import { TokenInterceptorService } from './services/token-interceptor.service';
+import { ErrorInterceptorService } from './services/error-interceptor.service';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideHttpClient(withInterceptorsFromDi()),
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: TokenInterceptorService,
+      multi: true,
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: ErrorInterceptorService,
+      multi: true,
+    },
+  ],
 };
