a047c9fce1f2e4fa761ec19ba746fd6b488fe6ec
DavidRodriguez <davisito544@gmail.com>
2024-08-26 02:37:26 -0500

    [Actualizacion] cambio en la forma de aplicar el router en la aplicacion

--- frontend/tierraBendicion/src/app/app.component.ts
+++ frontend/tierraBendicion/src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'tierraBendicion';
